ef87f0742827f20b9a90fc79f79d882bcddd3d59
Reinoptland <reinoptland@gmail.com>
2021-02-11 16:38:42 +0100

    Make request

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ console.log("MAIN!");
 
 async function whoLetTheDogsOut() {
   console.log("WOF", axios);
+  const response = axios.get("https://dog.ceo/api/breed/schnauzer/images");
+  console.log("DOGS??", response); // what??
 }
 
 whoLetTheDogsOut();
