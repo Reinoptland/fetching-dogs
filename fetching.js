97711b6d30e4cf8231240904a4501f607c2c7266
Reinoptland <reinoptland@gmail.com>
2021-02-11 15:47:49 +0100

    Display data to user

--- fetching.js
+++ fetching.js
@@ -4,9 +4,10 @@ const url = "https://api.kanye.rest?format=text";
 
 // parameter (urlToRequest)
 async function letKanyeSpeakWisdom(urlToRequest) {
-  console.log("HI!", urlToRequest);
+  //   console.log("HI!", urlToRequest);
   const response = await axios.get(urlToRequest);
-  console.log("STATUS:", response.status, "DATA:", response.data);
+  //   console.log("STATUS:", response.status, "DATA:", response.data);
+  console.log(`KANYE SAYS: ${response.data}`);
 }
 
 // argument
